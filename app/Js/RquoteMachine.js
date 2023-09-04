a57626540ec669a648020f501e9b317ee5c2454d
Tony Fang <tonyfang1897@gmail.com>
2023-09-04 15:33:33 -0400

    almost done

--- app/Js/RquoteMachine.js
+++ app/Js/RquoteMachine.js
@@ -1,11 +1,52 @@
-function func() {
-    fetch("Js/quotes.json")
-        .then((quotes) => {
-            return quotes.json();
-        })
-        .then(data => { console.log(data) })
+const file = "app/Js/quotes.json"
+let quotesArray
 
+function getQuotes() {
+    return fetch(file)
+        .then((response) => {
+            return response.json();
+        })
+        .then(data => {
+            quotesArray = data.quotes;
+            console.log(data);
+            console.log(quotesArray);
+        })
         .catch(error => {
             console.error("Error fetching JSON:", error);
         });
 }
+
+
+function rndQ() {
+    let random = Math.floor(Math.random() * quotesArray.length)
+    let randomQuote = quotesArray[random]
+    return randomQuote
+}
+
+function Q() {
+    randomQuote = rndQ()
+    $("#text").text(randomQuote.quote)
+    $("#author p").text(randomQuote.author)
+
+    let tweetUrl = "https://twitter.com/intent/tweet?hashtags=Quotes,StevenUniverse&text=" + encodeURIComponent(randomQuote.quote) + " - " + encodeURIComponent(randomQuote.author)
+    $("#tweet-quote").attr("href", tweetUrl)
+
+    let tumblrpostUrl = "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quote,StevenUniverse&content=" + encodeURI(randomQuote.quote) + "&caption=" + encodeURIComponent(randomQuote.author) + "&canonicalUrl=https://random-quote-machine.freecodecamp.rocks/"
+    $("#tumblr-post").attr("href", tumblrpostUrl)
+
+
+
+
+}
+
+
+
+$(document).ready(function () {
+    getQuotes().then(() => {
+        Q();
+    });
+
+    $("#new-quote").click(() => {
+        Q()
+    })
+});
