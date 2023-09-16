11bb7fee38a80c21edb6325360bd8703a69b2688
Tony Fang <tonyfang1897@gmail.com>
2023-09-16 19:55:11 -0400

    revised to vanilla javascript changed fetch to an async function created new js file to be filled with jquery soon

--- app/Js/RquoteMachine.js
+++ app/Js/RquoteMachine.js
@@ -1,19 +1,30 @@
 const file = "app/Js/quotes.json"
-let quotesArray
 
-function getQuotes() {
-    return fetch(file)
-        .then((response) => {
-            return response.json();
-        })
-        .then(data => {
-            quotesArray = data.quotes;
-            console.log(data);
-            console.log(quotesArray);
-        })
-        .catch(error => {
-            console.error("Error fetching JSON:", error);
-        });
+// async function getQuotes() {
+//     try {
+//         const response = await fetch(file);
+//         const data = await response.json();
+//         quotesArray = data.quotes;
+//         console.log(data);
+//         console.log(quotesArray);
+//     } catch (error) {
+//         console.error("Error fetching JSON:", error);
+//     }
+// }
+
+async function getQuotes() {
+    try {
+        response = await fetch(file)
+        data = await response.json()
+        quotesArray = data.quotes
+        console.log(quotesArray)
+    }
+    catch (error) {
+        console.log("Failed to obtain Json File", error)
+    }
+    finally {
+        console.log("Attempt to get Quotes Completed")
+    }
 }
 
 
@@ -25,28 +36,24 @@ function rndQ() {
 
 function Q() {
     randomQuote = rndQ()
-    $("#text").text(randomQuote.quote)
-    $("#author p").text(randomQuote.author)
+    document.querySelector("#text").textContent = randomQuote.quote
+    document.querySelector("#author p").textContent = randomQuote.author
 
     let tweetUrl = "https://twitter.com/intent/tweet?hashtags=Quotes,StevenUniverse&text=" + encodeURIComponent(randomQuote.quote) + " - " + encodeURIComponent(randomQuote.author)
-    $("#tweet-quote").attr("href", tweetUrl)
+    document.querySelector("#tweet-quote").setAttribute("href", tweetUrl)
 
     let tumblrpostUrl = "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quote,StevenUniverse&content=" + encodeURI(randomQuote.quote) + "&caption=" + encodeURIComponent(randomQuote.author) + "&canonicalUrl=https://random-quote-machine.freecodecamp.rocks/"
-    $("#tumblr-post").attr("href", tumblrpostUrl)
-
-
-
+    document.querySelector("#tumblr-post").setAttribute("href", tumblrpostUrl)
 
 }
 
-
-
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function () {
     getQuotes().then(() => {
-        Q();
-    });
-
-    $("#new-quote").click(() => {
         Q()
     })
-});
+
+    document.querySelector("#new-quote").addEventListener("click", () => {
+        Q()
+    })
+
+})
---
+++ app/Js/RquoteMachineJQ.js
@@ -0,0 +1,3 @@
+const file = "app/Js/quotes.json"
+const link = "https://gist.githubusercontent.com/dragonofnyc0218/367842b9afc9ac978e06a79fa863ef61/raw/365dab51187b9ccab5ed92a30cb4fcaaade40155/gistfile1.txt"
+
